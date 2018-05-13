4027faed88e0d80ddd4cda792838fc13c6569acc
a.kazemi.2017 <arashkd@hotmail.com>
2018-05-13 21:27:49 +0200

    Definitive final project

--- openfda-project/server_project.py
+++ openfda-project/server_project.py
@@ -16,28 +16,28 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         if self.path == "/":
-            with open("openfda_project.html") as f:
+            with open("openfda.html") as f:
                 message = f.read()
                 self.wfile.write(bytes(message, "utf8"))
 
         elif "searchDrug" in self.path:
             params = self.path.split("?")[1]
             drug = params.split("&")[0].split("=")[1]
-            limit = params.split("&")[1].split("=")[1]
+            #limit = params.split("&")[1].split("=")[1]
             headers = {'User-Agent': 'http-client'}
 
             conn = http.client.HTTPSConnection("api.fda.gov")
-            conn.request("GET", '/drug/label.json?search=active_ingredient:' + drug + "&limit=" + limit, None, headers)
+            conn.request("GET", '/drug/label.json?search=active_ingredient:' + drug + "&limit=10", None, headers)
             r1 = conn.getresponse()
             print(r1.status, r1.reason)
             repos_raw = r1.read().decode("utf-8")
             conn.close()
 
             drugs = json.loads(repos_raw)
-            total_drugs = ""
+            total_drugs = "<ol>"
             for drug in drugs['results']:
-                drugs_info = "<ol>" + "Drug Id: " + drug['id'] + "</ol>"
-                total_drugs = total_drugs + drugs_info
+                total_drugs += "<li>" + "Drug Id: " + drug['id']
+                total_drugs += "</li>"
 
             self.wfile.write(bytes(total_drugs, "utf8"))
 
@@ -45,11 +45,11 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         elif "searchCompany" in self.path:
             params = self.path.split("?")[1]
             company = params.split("&")[0].split("=")[1]
-            limit = params.split("&")[1].split("=")[1]
+            #limit = params.split("&")[1].split("=")[1]
             headers = {'User-Agent': 'http-client'}
 
             conn = http.client.HTTPSConnection("api.fda.gov")
-            conn.request("GET", '/drug/label.json?search=openfda.manufacturer_name:' + company + "&limit=" + limit, None, headers)
+            conn.request("GET", '/drug/label.json?search=openfda.manufacturer_name:' + company + "&limit=10", None, headers)
             r1 = conn.getresponse()
             print(r1.status, r1.reason)
             repos_raw = r1.read().decode("utf-8")
@@ -157,6 +157,7 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                         "</html>"
 
             self.wfile.write(bytes(warninglist, "utf8"))
+
 
 
         return
